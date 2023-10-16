7448bccf056db34f414405d19965fdb8695934a8
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-16 20:31:23 -0300

    add string function to show model title on admin painel

--- games/models.py
+++ games/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    platform = models.CharField(max_length=100)
+    producer = models.CharField(max_length=100)
+    release_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
