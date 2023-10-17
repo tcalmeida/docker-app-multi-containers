514202b7a6f9ef63bddd4a186166e6b04dbc65c2
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-16 21:26:34 -0300

    create game-create url path

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import game_list
+from .views import game_list, create_game
 
 urlpatterns = [
-    path('game-list/', game_list, name='game-list')
+    path('game-list/', game_list, name='game-list'),
+    path('game-create/', create_game, name='create-game')
 ]
