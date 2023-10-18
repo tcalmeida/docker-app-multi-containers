95f07dc2ff01d9cc0ce6b62db5e385b064d639df
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-18 20:21:51 -0300

    refactor: change url name for better understanting | games-list

--- games/urls.py
+++ games/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import list_games, list_game, create_game, update_game, delete_game
 
 urlpatterns = [
-    path('game-list/', list_games, name='game-list'),
+    path('games-list/', list_games, name='games-list'),
     path('list-game/<str:pk>/', list_game, name='list-game'),
     path('game-create/', create_game, name='create-game'),
     path('update-game/<str:pk>/', update_game, name='update-game'),
