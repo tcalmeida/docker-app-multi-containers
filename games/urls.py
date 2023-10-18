3f66db7a0019417598a3960a15b7d0c9d8cde854
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-18 19:42:43 -0300

    create update and delete urls paths

--- games/urls.py
+++ games/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import game_list, create_game
+from .views import game_list, create_game, game_detail, update_game, delete_game
 
 urlpatterns = [
     path('game-list/', game_list, name='game-list'),
-    path('game-create/', create_game, name='create-game')
+    path('game-create/', create_game, name='create-game'),
+    path('list-game/<str:pk>/', game_detail, name='list-game'),
+    path('update-game/<str:pk>/', update_game, name='update-game'),
+    path('delete-game/<str:pk>/', delete_game, name='delete-game'),
 ]
