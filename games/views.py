7b895481eaa13fb979102d995f5993aed8e87fdf
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-18 19:35:26 -0300

    create update-game function view

--- games/views.py
+++ games/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,7 +11,7 @@ from .serializers import GameSerializer
 def game_list(request):
     games = Game.objects.all()
     serializer = GameSerializer(games, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -19,4 +20,30 @@ def create_game(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT'])
+def game_detail(request, pk):
+    # list one game
+    try:
+        game = Game.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = GameSerializer(game)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def update_game(request, pk):
+    try:
+        game = Game.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    serializer = GameSerializer(instance=game, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
