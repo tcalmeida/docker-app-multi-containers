fa9b18fb1ba3832b81d06ab7ea9c6caad7b8b4eb
Thiago Almeida <113650703+tcalmeida@users.noreply.github.com>
2023-10-18 21:49:31 -0300

    include frontend urls

--- games_api/urls.py
+++ games_api/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('games.urls'))
+    path('', include('games.urls')),
+
+    path('', include('frontend.urls'))
 ]
