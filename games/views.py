2027899a8d8f4668d098081be9ed1be0e84daa57
Thiago Almeida <tcalmeida85@gmail.com>
2024-01-04 12:06:02 -0300

    create home view

--- games/views.py
+++ games/views.py
@@ -1,4 +1,5 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,6 +7,8 @@ from .models import Game
 from .serializers import GameSerializer
 
 
+def home(request):
+    return render(request, "index.html")
 # list all games
 @api_view(['GET'])
 def list_games(request):
